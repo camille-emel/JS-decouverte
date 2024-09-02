9aaefb4895baed39163b1e285ea12833217e9fdc
camille emel < ser.email>
2024-09-02 15:41:23 +0200

    exo5

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,15 @@
 // you can write js here
 
 console.log('exo-5');
+let input  = prompt("Quel phrase voulais vous tester ?").toUpperCase()
+let vowels  = ["A", "E", "I", "O", "U"];
+let resultArray= []
+for(let i = 0; i < input.length; i++){
+    for(let j = 0; j < vowels.length; j++){
+        if (input[i] === vowels[j]){
+            resultArray.push(vowels[j]);
+        }
+    }
+}
+console.log(resultArray.join(""));
+
