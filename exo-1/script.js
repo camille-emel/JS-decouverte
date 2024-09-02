b1cffb0a37c02c48ed4af770511a52aefee2eb15
camille emel < ser.email>
2024-09-02 12:03:49 +0200

    exo2

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,8 +1,8 @@
 // you can write js here
-// let kelvins = 294
-var kelvins = prompt("Quelle est la température en Kelvins aujourd'hui ?");
+let kelvins = 294
+kelvins = prompt("Quelle est la température en Kelvins aujourd'hui ?");
 let celcius = kelvins - 273;
 //Formule de calcule pour USA
-let farhen = celcius * (9/5) +32;
+let farhen = celcius * (9/5) + 32;
 //Transforme la variable farhen en Int
 console.log("La température en fahrenheits est de : " + Math.floor(farhen));
--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,54 @@
 // you can write js here
-console.log('exo-2');
+let isTesting  = Boolean(false)
+
+const myDate = new Date();
+const jourSemaine = "Jour de semaine";
+const jourWeekend = "Jour de Weekend";
+
+let today = myDate;
+let hour = today.getHours();
+
+
+//Jour de la semaine 0-6 avec, 0 = dimanche
+if (isTesting){
+     let fakeDay = prompt("Quel jour ?").toLowerCase();
+    switch (fakeDay) {
+        case "sunday":
+        case "dimanche":
+            day = 0;
+            break;
+        case "monday":
+        case "lundi":
+            day = 1;
+            break;
+        case "tuesday":
+        case "mardi":
+            day = 2;
+            break;
+        case "wednesday":
+        case "mercredi":
+            day = 3;
+            break;
+        case "thursday":
+        case "jeudi":
+            day = 4;
+            break;
+        case "friday":
+        case "vendredi":
+            day = 5;
+            break;
+        case "saturday":
+        case "samedi":
+            day = 6;
+            break;
+    }
+    let fakeHour = prompt("Quel heure ?");
+    today = day;
+    hour = fakeHour;
+}
+if (today === 0 || today === 6 || (today === 5 && hour > 17 ) || (today === 1 && hour < 9 )) {
+    console.log(jourWeekend)
+}
+else {
+    console.log(jourSemaine)
+}
