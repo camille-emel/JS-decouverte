10e2e6400a696c87faf49ddf7d6e035704b93a99
camille emel < ser.email>
2024-09-02 14:57:36 +0200

    exo4

--- exo-4/script.js
+++ exo-4/script.js
@@ -3,3 +3,21 @@
 console.log('exo-4');
 
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+
+secretMessage.pop();
+
+secretMessage.push("to", "program");
+
+secretMessage[secretMessage.indexOf("easily")]= "right"
+
+secretMessage.shift();
+
+secretMessage.unshift("Programming");
+
+secretMessage.splice(secretMessage.indexOf("get"), 5, "know")
+
+console.log(secretMessage.join(" "));
+
+
+
+
